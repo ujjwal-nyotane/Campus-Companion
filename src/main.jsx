85ad3ea733a44de7e861be2c14eb60720ec247ba
Ujjwal Nyotane <ujjwaljaat5556@gmail.com>
2026-08-07 02:49:15 +0530

    imprroved css

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ const accessform=<div className='accessform'>
       </form>
     </div>
 const welcomemessage = <div style={{padding:"35px",textAlign:"center",verticalAlign:"center"}}>
-    <h2 >Welcome to Campus Companion</h2>
+    <h2 className='accesspagetitle' >Welcome to <span style={{color:"rgb(208, 174, 5)"}}>Campus</span> <span style={{color:"rgb(0, 67, 210)"}}>Companion</span></h2>
     <p>Campus-Companion is a smart student portal designed to simplify campus life. It provides attendance tracking, timetables, academic results, notices, assignments, event updates, fee information and others in one place. The platform helps students stay organized, connected, and informed through an intuitive and user-friendly interface.</p>
     </div>
 const nav = <nav className='navbar'>
@@ -21,14 +21,31 @@ const nav = <nav className='navbar'>
       <img src='src/assets/logo/logo.png' alt='logo'/>
       <img  className='logotext' src='src/assets/logo/logotext.png' alt='logotext'/>
   </div>
-  <div>
-
+  {/* <div className='navigate'>
+    <div className='home'>
+      <img src='src/assets/logo/home.png'/>
+    <span>Home</span>
+    </div> 
+    <div className='home'>
+      <img src='src/assets/logo/attendance.png'/>
+    <span>Attendance</span>
+    </div>
+    <div className='home'>
+      <img src='src/assets/logo/result.png'/>
+    <span>Result</span>
+    </div>
+    <div className='home'>
+      <img src='src/assets/logo/food.png'/>
+    <span>Food</span>
+    </div> 
   </div>
   <div>
-
-  </div>
+  <span>Your profile</span>
+  </div>*/}
 </nav>
+const footer= <footer>
 
+</footer>
 
 const home = <div className='bg-primary'>
   {nav}
@@ -36,6 +53,7 @@ const home = <div className='bg-primary'>
   <div className='grid-2'>
     {welcomemessage}
     {accessform}
+    {footer}
   </div>
   </main>
   
