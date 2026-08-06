e0b08fee1b10bc7ef7723f9a6f090cd56d6ca14b
Ujjwal Nyotane <ujjwaljaat5556@gmail.com>
2026-08-06 15:20:17 +0530

    started working on login page and nav bar

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,40 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-const loginform=<div>
+const accessform=<div className='accessform'>
       <h3 className='text text-center'>Login / Signup</h3>
-      <form>
-        <div className='container'><span>Login</span>  <span>Signup</span></div>
-
+      <form className='container text-center'>
+        <div ><span>Login</span>  <span>Signup</span></div>
+        <label for='login-email' ></label>
+          <input type="email" id="login-email" placeholder='Your Mail Address'/><br/>
+          <label for='login-password' ></label>
+          <input type="password" id="login-password" placeholder='Your Password'/>
       </form>
     </div>
+const welcomemessage = <div style={{padding:"35px",textAlign:"center",verticalAlign:"center"}}>
+    <h2 >Welcome to Campus Companion</h2>
+    <p>Campus-Companion is a smart student portal designed to simplify campus life. It provides attendance tracking, timetables, academic results, notices, assignments, event updates, fee information and others in one place. The platform helps students stay organized, connected, and informed through an intuitive and user-friendly interface.</p>
+    </div>
+const nav = <nav className='navbar'>
+  <div className='logo'>
+      <img src='src/assets/logo/logo.png' alt='logo'/>
+      <img  className='logotext' src='src/assets/logo/logotext.png' alt='logotext'/>
+  </div>
+  <div>
+
+  </div>
+  <div>
+
+  </div>
+</nav>
+
 
 const home = <div className='bg-primary'>
+  {nav}
   <main >
-  <div className='container grid-2'>
-    <div style={{padding:"35px",textAlign:"center",verticalAlign:"center"}}>
-    <h2 className='text'>Welcome to Campus Compainion</h2>
-    <p className='text'>Campus-Companion is a smart student portal designed to simplify campus life. It provides attendance tracking, timetables, academic results, notices, assignments, event updates, fee information and others in one place. The platform helps students stay organized, connected, and informed through an intuitive and user-friendly interface.</p>
-    </div>
-    {loginform}
+  <div className='grid-2'>
+    {welcomemessage}
+    {accessform}
   </div>
   </main>
   
