9337c1aac31a2e58025a8ff123bfff54a357768f
Ujjwal Nyotane <ujjwaljaat5556@gmail.com>
2026-08-14 00:16:26 +0530

    created components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import Dashboard from "./components/dashboard.jsx";
+
+function App(){
+    return (
+        <Dashboard/>
+    )
+}
+export default App
---
+++ frontend/src/components/dashboard.jsx
@@ -0,0 +1,4 @@
+function Dashboard() {
+return (<h1>hello</h1>)
+}
+ export default Dashboard
