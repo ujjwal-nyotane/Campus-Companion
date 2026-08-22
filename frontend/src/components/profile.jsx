2fdc388f401cb96339deed4612dc04b0cab47847
Ujjwal <ujjwalgandhi38@gmail.com>
2026-08-22 23:31:53 +0530

    profile page merging files

--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -1,6 +1,6 @@
 import './profile.css'
 
-function Profile(){
+function Profile() {
     return <>
         <div className='page-header'>
             <p className='head'>STUDENT INFO</p>
@@ -9,11 +9,43 @@ function Profile(){
         </div>
         <div className='main-content'>
             <div className='profile-hero'>
-                <div className='p-photo'>U</div>
-                <div className='p-info'>
-                    <h2 className='s-firstname'>Ujjwal</h2>
-                    <div className='branch'>
-                        <span>CSE-AIML <b>&#183;</b> 3rd Semester</span>
+                <div className='np'>
+                    <div className='p-photo'>U</div>
+                    <div className='p-info'>
+                        <h1 className='s-firstname'>Ujjwal</h1>
+                        <div className='branch'>
+                            <span>CSE-AIML <b>&#183;</b> 3rd Semester</span>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='student-stats'>
+                    <div className='s-stats'>
+                        <div className='d'>
+                            9.34
+                        </div>
+                        <div className='l'>
+                            CGPA
+                        </div>
+
+                    </div>
+                    <div className='s-stats'>
+                        <div className='d'>
+                            87%
+                        </div>
+                        <div className='l'>
+                            Attendance
+
+                        </div>
+
+                    </div>
+                    <div className='s-stats'>
+                        <div className='d'>
+                            3
+                        </div>
+                        <div className='l'>
+                            Semester
+                        </div>
 
                     </div>
                 </div>
@@ -35,7 +67,7 @@ function Profile(){
 
             </div>
         </div>
-    
+
     </>
 }
 
